55a5a44a98f3fcf619163629d77d44b6ec7d9747
Tkendi <acg70159@gmail.com>
2020-08-11 21:34:03 +0900

    weather module

--- src/components/modules/weather.js
+++ src/components/modules/weather.js
@@ -1,10 +1,38 @@
-import { FETCH_FLIGHT } from "../../lib/api/index";
+import { createAction, handleActions } from "redux-actions";
+import createRequestSaga, {
+  createRequestActionTypes,
+} from "../../lib/createRequestSaga";
+import * as weatherAPI from "../../lib/api/index";
+import {takeLatest} from 'redux-saga/effects'
 
-export default function (state = [], action) {
-  switch (action.type) {
-    case FETCH_FLIGHT:
-      console.log(action);
-      return [action.payload.data, ...state];
-  }
-  return state;
+const [WEATHER, WEATHER_SUCCESS, WEATHER_FAILURE] = createRequestActionTypes(
+  "weather/WEATHER"
+);
+
+export const weatherRead = createAction(WEATHER, (nx, ny) => (nx, ny));
+
+const readWeatherSaga = createRequestSaga(WEATHER, weatherAPI.weather);
+export function* weatherSaga() {
+  yield takeLatest(WEATHER, readWeatherSaga);
 }
+
+const initialState = {
+  data: null,
+  error: null,
+};
+
+const weather = handleActions(
+  {
+    [WEATHER_SUCCESS]: (state, { payload: data }) => ({
+      ...state,
+      data,
+    }),
+    [WEATHER_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      error,
+    }),
+  },
+  initialState
+);
+
+export default weather;
--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,15 +1,31 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const weatherData = async (nx, ny) => {
-  const data = await axios.get(`http://localhost:4000/api?nx=${nx}&ny=${ny}`);
+// export const weatherData = async (nx, ny) => {
+//   const data = await axios.get(`http://localhost:4000/api?nx=${nx}&ny=${ny}`);
 
-  const info = {};
-  info.baseTime = data.data.baseTime;
-  info.baseDate = data.data.baseDate;
+//   const info = {};
+//   info.baseTime = data.data.baseTime;
+//   info.baseDate = data.data.baseDate;
 
-  info.TMP = data.data.T1H;
-  info.RNH = data.data.RN1;
-  info.SKY = data.data.PTY;
+//   info.TMP = data.data.T1H;
+//   info.RNH = data.data.RN1;
+//   info.SKY = data.data.PTY;
 
-  return info;
-};
+//   return info;
+// };
+
+
+// import axios from 'axios'
+
+// export const FETCH_FLIGHT = 'FETCH_FLIGHT';
+
+// export function getAllFlights() {
+//   const request = axios.get('http://localhost:4000/api?nx=60&ny=127')
+
+//   console.log(request)
+// }
+
+import client from './client'
+
+export const weather = (nx, ny) => 
+  client.get('http://localhost:4000/api?nx=${nx}&ny=${ny}')
