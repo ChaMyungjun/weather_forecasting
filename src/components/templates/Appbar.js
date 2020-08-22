fcd7b36a287d4fea71416918c69eae7e1b176ad0
Tkendi <acg70159@gmail.com>
2020-08-22 18:55:30 +0900

    Appbar create

---
+++ src/components/templates/Appbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  IconButton,
+  makeStyles,
+  createMuiTheme,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+    },
+    secondary: {
+      light: "#ff7961",
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+const Appbar = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="secondary">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            The Weather
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Appbar;
